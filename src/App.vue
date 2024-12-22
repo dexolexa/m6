ab6689cc24056f7b28e846d8f919716a687ac39e
alexey <oleksiy.platov@yandex.ru>
2024-12-22 20:43:17 +0500

    hello

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HeAdEr from "./components/HeAdEr.vue";
+import Main from "./views/Main.vue";
+</script>
+
+<template>
+  <main><HeAdEr /> <p><Main /></p></main>
+</template>
---
+++ src/views/Main.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+
+            Главная Страница
+    </section>
+</template>
